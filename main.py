2e2a4b1273de919b3574264003dbcbafd4f85481
Cole Wilson <cole@colewilson.xyz>
2021-11-22 10:39:47 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
+import qrcode
 import requests
+from io import BytesIO, StringIO
 from flask_cors import CORS, cross_origin
 from flask import Flask, redirect, request
 
@@ -7,14 +9,25 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+def serve_image(pil_img):
+    img_io = StringIO()
+    pil_img.save(img_io, 'JPEG', quality=70)
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg')
+
 @app.after_request
 def apply_text_only(response):
     response.headers["Content-Type"] = "text/plain"
     return response
 
 @app.errorhandler(403)
-def forbidden(e):
-    return '403 forbidden'
+def forbidden(e): return '403 forbidden'
+
+@app.errorhandler(500)
+def forbidden(e): return '500 error'
+
+@app.errorhandler(404)
+def forbidden(e): return '404 not found'
 
 @app.route('/')
 def root():
@@ -34,4 +47,36 @@ def shorten_url():
   else:
     abort(403)
 
+@app.route('qr')
+@corss_origin()
+def make_qr():
+    background = request.args.get("bg", "white")
+    foreground = request.args.get("fg", "black")
+    inside = request.args.get("inside", None)
+    size = request.args.get("size", 20)
+    border = request.args.get("border", 2)
+    data = request.args.get("data")
+    error_correction = qrcode.constants.ERROR_CORRECT_L
+    
+    if inside is not None: error_correction = qrcode.constants.ERROR_CORRECT_H
+
+    qr = qrcode.QRCode(version=size, error_correction=error_correction, box_size=10, border=border)
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color=foreground, back_color=background)
+    
+    if inside is not None:
+        w, h = round(0.25 * img.width), round(0.25 * img.height)
+        brand = requests.get(inside)
+        brand = Image.open(BytesIO(brand.content))
+        brand = brand.resize((w, h))
+        xmin = ymin = int((width / 2) - (logo_size / 2))
+        xmax = ymax = int((width / 2) + (logo_size / 2))
+        img.paste(brand, (xmin, ymin, xmax, ymax))
+        
+    return serve_image(img)
+    
 app.run(port=80, host="0.0.0.0")
+    
+    
+    
